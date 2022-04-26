0a8a55fea7bc57ba544fff65623ddb31c25dcc0d
principles31 <principles31@gmail.com>
2022-04-26 15:25:56 +0000

    Add main.js file

---
+++ main.js
@@ -0,0 +1,38 @@
+const speakersInforArray = [
+    {
+      image: 'img/spk/Devlin_Duldulao.jpg',
+      name: 'Fabian Gosebrink',
+      details: 'Senior Consultant AIT GmbH & Co. KG',
+      work: 'Microsoft .NET, DevOps, Cloud  IoT developerin Software engineer',
+    },
+    {
+      image: 'img/spk/Florian_Bader.jpg',
+      name: 'Nate Finch',
+      details: 'Senior WordPress Engineer at Strattic.com',
+      work: 'He helps build serverless integrations for WordPress',
+    },
+    {
+      image: 'img/spk/James_Bender.jpg',
+      name: 'Nicolas Fränkel',
+      details: 'Developer Advocate',
+      work: 'Currently working for Hazelcast. Also double as a teacher in universities and higher education schools, a trainer and triples as a book author.s',
+    },
+    {
+      image: 'img/spk/John Walker.jpg',
+      name: 'Facundo Giuliani',
+      details: 'Developer Relations Engineer',
+      work: 'Full Stack Developer. Auth0 Ambassador. Prisma Ambassador. Cloudinary Media Developer Expert. GitKraken Ambassador. He is also an open-source contributor',
+    },
+    {
+      image: 'img/spk/pavel danilyuk.jpg',
+      name: 'Fabian Gosebrink',
+      details: 'Offering Solutions Software GmbH',
+      work: 'Fabian Gosebrink ist Pluralsight Autor,Microsoft MVP und ASP.NET Core & Angular Webentwickler',
+    },
+    {
+      image: 'img/spk/Roy Derks.jpg',
+      name: 'George Kosmidis',
+      details: 'Cloud Solutions Architect',
+      work: 'He is a Cloud Solutions Architect and.NET Software Engineer',
+    },
+];
