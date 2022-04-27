d15c7a1af7cfb27735577bd7d7346550ad96493b
principles31 <principles31@gmail.com>
2022-04-27 13:17:01 +0000

    Add about.js file

---
+++ about.js
@@ -0,0 +1,37 @@
+const humbergur = document.querySelector('.humber');
+const menuList = document.querySelector('.desk_menu');
+const navbar = document.querySelector('.second_nav');
+const listItems = document.querySelectorAll('.desk_menu_list');
+const closeMenu = document.querySelector('.closeMenu');
+
+/* reset menu function */
+
+function resetMenu() {
+  listItems.forEach((item) => item.classList.remove('show'));
+  humbergur.classList.remove('hidden');
+  menuList.classList.remove('ul_list');
+  navbar.classList.remove('expand');
+  closeMenu.classList.remove('close_show');
+}
+
+humbergur.addEventListener('click', () => {
+  listItems.forEach((item) => item.classList.remove('hidden'));
+  listItems.forEach((item) => item.classList.add('show'));
+
+  humbergur.classList.add('hidden');
+  menuList.classList.add('ul_list');
+  navbar.classList.add('expand');
+  closeMenu.classList.add('close_show');
+
+//  listItems.forEach((item) => item.addEventListener('click', resetMobileMenu));
+});
+
+closeMenu.addEventListener('click', resetMenu);
+
+function resizeWindow() {
+  if (window.innerWidth > 768) {
+    resetMenu();
+  }
+}
+
+window.addEventListener('resize', resizeWindow);
